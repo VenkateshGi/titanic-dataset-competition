882bfe95de61d63b1a55b9c82a0011620ced4a36
G.Venkatesh <garnepudi.venkatesh@gmail.com>
2019-02-13 21:33:42 +0530

    edited py file

--- titanic-survival-predictions.py
+++ titanic-survival-predictions.py
@@ -1,51 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import seaborn as sns
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 import os
 print(os.listdir("../input"))
-
+#list the downloaded csv file from kaggle in the current directory 
+#, so that you can read the bothtrain.csv and test.csv files
 # Any results you write to the current directory are saved as output.
-
-
-# In[2]:
-
 
 titanic_train = pd.read_csv("../input/train.csv")
 titanic_test = pd.read_csv("../input/test.csv")
 
-
-# let's check some details
-
-# In[3]:
-
-
 titanic_train.info()
-
-
-# In[4]:
-
 
 titanic_test.info()
 
 
 # As we can see, there are null values in both train adn test datasets
-
-# In[5]:
-
 
 #Let's set the survived to -1 and then we combine both datasets
 titanic_test["Survived"] = -1
